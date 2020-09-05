ceb9021aa839353db15fc4c3a91df0f8959e843c
diankra <56646341+diankra@users.noreply.github.com>
2020-09-05 14:29:09 +0200

    Añadido movimiento
    El cubo se mueve en dos direcciones y salta

---
+++ Plataformas/Assets/Scripts/mainCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCharacter : MonoBehaviour
+{
+    public float movementSpeed = 2;
+    public float jumpHeight = 600f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Change the color to blue
+        gameObject.GetComponent<Renderer>().material.color = Color.blue; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //to test movement ERASE WHEN MOBILE CONTROLS
+        if (Input.GetKey(KeyCode.A)) {
+            moveLeft();
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            moveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+            rigidBody.AddForce(transform.up * jumpHeight);
+        }
+    }
+
+    public void moveLeft() {
+        transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+    }
+
+    public void moveRight() {
+        transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+    }
+}

8c2eb39a73010142f4d6f81b8933c264ff3e3f08
diankra <56646341+diankra@users.noreply.github.com>
2020-09-07 13:27:17 +0200

    Añadidos coleccionables y enemigos
    Los enemigos matan directamente y los coleccionables se cuentan

---
+++ Plataformas/Assets/Scripts/Collectable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private static int collected = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Change the color to green
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Main Cube")
+        { 
+            collected += 1;
+            Debug.Log("Collecionados: " + collected);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Plataformas/Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public float distanceRight;
+    public float distanceLeft;
+    public float speed;
+    private float initPosition;
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Change the color to red
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        //Set the initial position. It will only move on the horizontal (X) axis, so only the X position is relevant for this
+        initPosition = transform.position.x;
+
+        //Initialize the RigidBody component
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //these checks are duplicate code, but they do avoid the enemy from getting stuck when its speed is too high and can't go back
+        //to Right or Left position
+        if (transform.position.x >= initPosition + distanceRight)
+        {
+            if (speed > 0)
+            {
+                speed = speed * -1;
+            }
+        }
+        else if (transform.position.x <= initPosition - distanceLeft)
+        {
+            if (speed < 0)
+            {
+                speed = speed * -1;
+            }
+        }
+        rigidBody.velocity = new Vector3(speed, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Main Cube")
+        {
+            //this check could be skipped if we were sure only the player will fall down
+            SceneManager.LoadScene("deathScene");
+        }
+        else //if it hits a collision, turns to the other direction
+        {
+            speed = 0 - speed;
+        }
+    }
+}
